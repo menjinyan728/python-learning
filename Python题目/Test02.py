3efce270c4ea5f401f29bcf90418b04127bfeeea
menjinyan <menjin728@gmail.com>
2024-09-10 19:43:48 +0800

    提交

---
+++ Python题目/Test02.py
@@ -0,0 +1,8 @@
+#要求：使用print函数从键盘输出姓名，年龄，座右铭并使用print函数输出到控制台
+name=input('请输入你的姓名:')
+age=input('请输入你的年龄:')
+motto=input('请输入你的座右铭:')
+print('------自我介绍 ------')
+print('姓名:',name)
+print('年龄',age)
+print('座右铭',motto)
---
+++ python/Demo10代码缩进.py
@@ -0,0 +1,12 @@
+#一般代码不需要缩进
+print("hello")
+print("world")
+
+#需要缩进的
+#1、类的定义
+class studednt:
+    pass
+
+#函数的定义
+def fun():
+    pass
